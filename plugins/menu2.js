cbd025bf474ad2fd7668177c99231b7159e7a38b
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-08-05 13:04:52 -0300

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -7,7 +7,7 @@ let m2 = `
 ≡ _Use estos comandos sin el prefijo_
 
 ▢ Si tienes más  audios  
-envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
+envíame por wa.me/5493704583368 el *audio + comando* con en el que responderá
 *No* _voces de lolis_
 
 
@@ -22,9 +22,9 @@ envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
 └──────────────`
 const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     
-    conn.sendHydrated(m.chat, m2, '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, 'https://youtube.com/fg98f', 'YouTube', null, null, [
+    conn.sendHydrated(m.chat, m2, '▢ NixiV4  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\n', pp, 'https://youtube.com', 'YouTube', null, null, [
      ['⏍ Info', '/botinfo'],
-      ['⌬ Grupos', '/gpdylux']
+      ['⌬ opcion', '/beta']
     ], m)
     
 }

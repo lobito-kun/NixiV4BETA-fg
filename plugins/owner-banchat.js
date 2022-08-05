c86ff43edb8a344bc3a98fec9a6f74014299c640
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-08-05 13:10:46 -0300

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✅ Se desactivo el Bot en este grupo')
+    m.reply('✅ Se desactivo a Nixi en este grupo')
     // } else m.reply('Aquí hay un número de host...')
 }
 handler.help = ['banchat']
